f2151ee9f0d514f16eff053d7690f9eecf371789
Anbarasan M <161844754+Anbu-2005@users.noreply.github.com>
2025-06-08 22:10:43 +0530

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"https://todo-list-website-backend-28iq.onrender.com"
 })
 export default instance
